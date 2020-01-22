a03a5d957be5eb4fc2dc8a4a60439eb680d06a73
Matt Pignatore <mattpigs@gmail.com>
2020-01-22 18:21:10 -0500

    adding the build folder

---
+++ build/precache-manifest.94a81be7eb8cbffc5ec9200e8436d761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8252ae2cdc226e121b0342b6e589cb8e",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "d35fc353c54626f7560e",
+    "url": "/clicky/static/css/main.1333ccd3.chunk.css"
+  },
+  {
+    "revision": "344a9834bbae2f3c72ae",
+    "url": "/clicky/static/js/2.c4367b2d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky/static/js/2.c4367b2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d35fc353c54626f7560e",
+    "url": "/clicky/static/js/main.dc3c6d94.chunk.js"
+  },
+  {
+    "revision": "03845033a8934ae5952d",
+    "url": "/clicky/static/js/runtime-main.74279dd7.js"
+  }
+]);
